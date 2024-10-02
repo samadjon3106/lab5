f347d17f79efac29547a0501af9c63ce91ed3166
samadjon3106 <s.sayfullayev@newuu.uz>
2024-10-02 12:48:38 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*double windchilltemperature (double ta , double v) {
+    return 35.74 + 0.625 * ta - 35.75 * pow(v,0.16) + 0.4275 * ta * pow(v,0.16);
+}
+void problem1(double ta, double v){
+    cout<< "Enter outside temperature measured in degrees Fahrenheit: ";
+    cin>>ta;
+    cout<<"Enter the speed measured in miles per hour: ";
+    cin>>v;
+
+    if (ta <-58|| ta>41){
+        cout<<" Temperature must be between -58F and 41F.\n";
+        return;
+
+    }
+    if (v<2){
+        cout<<"Wind speed must be greater than 2pmh.\n";
+        return;
+    }
+
+
+    double tws=windchilltemperature(ta, v);
+    cout<<tws;
+}*/
+int main(){
+    /* double ta;
+    double v;
+    problem1(ta, v); */
+/*int a, b, c;
+cin>> a>>b>>c;
+if (a%b==0 && a%c==0) {
+    cout << b<<" and " << c << " are factors of " << a;
+    } else if (a % b != 0 && a % c == 0) {
+        cout << b << " is not factor of" << a << " but " << c << " is factor of " << a;
+    } else if (a % b == 0 && a % c != 0) {
+            cout << a << " is factor of " << a << ", but " << c << " is not ";
+        } else {
+            if (a % b != 0 && a % c != 0) {
+                cout << " None of them are factor of" << a;
+            }
+        } */
+
+
+ /*int a=0;
+int b=1;
+int n;
+cout<< "Enter a positive integer (n): ";
+cin>>n;
+cout<<a;
+if (n>0) {
+    cout << " , " << b;
+}
+for(int i=2; i<=n ; i++){
+    int next=a+b;
+  cout<<" , "<<next;
+  a=b;
+  b=next;
+
+}
+cout<<endl; */
+ int value;
+ cin>> value;
+ dig_1=value
+
+
+
+ return 0;
+
+}
